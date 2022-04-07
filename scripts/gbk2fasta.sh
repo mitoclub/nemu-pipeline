c0f8e56d60361e5ce1b6ba7e4d7faf9e43841d37
kpotoh <axepon@mail.ru>
2022-04-07 16:06:06 +0200

    gbk script

--- scripts/gbk2fasta.sh
+++ scripts/gbk2fasta.sh
@@ -2,5 +2,5 @@
 
 for file in $@ ; do
     echo $file
-    python -c "from Bio import SeqIO; SeqIO.convert($file, genbank, ${file%.*}.fasta, fasta);"
+    python3.8 -c "from Bio import SeqIO; SeqIO.convert(\"$file\", genbank, ${file%.*}.fasta, fasta);"
 done
