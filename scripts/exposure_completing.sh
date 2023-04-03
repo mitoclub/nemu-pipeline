e5fdc8d85c22c790838253be71d2ccdb7af6a7fa
kpotoh <axepon@mail.ru>
2023-04-03 22:16:15 +0300

    exposure again

--- scripts/exposure_completing.sh
+++ scripts/exposure_completing.sh
@@ -19,7 +19,7 @@ echo "Input directory: $indir"
 mkdir -p $indir/ms $indir/pyvolve/out
 
 method=iqtree
-replics=200
+replics=100
 GENCODE=2
 
 raw_tree=$indir/iqtree_anc_tree.nwk
@@ -37,7 +37,6 @@ fi
 
 tree=$indir/pyvolve/tree.nwk
 mulal=$indir/pyvolve/mulal.fasta
-log_file=$indir/pyvolve/pyvolve_${method}.log
 
 if [ ! -f ${indir}/observed_mutations_iqtree.tsv ] || 
 	[ ! -f ${indir}/exp_muts_invariant.tsv ] || 
@@ -53,8 +52,8 @@ echo All input files are exits. Start computing
 ################################################################################
 
 echo "Processing spectra calculation..."
-calculate_mutspec.py -b ${indir}/observed_mutations_iqtree.tsv -e ${indir}/exp_muts_invariant.tsv -o $indir/ms \
-    --exclude OUTGRP,ROOT --proba  --syn --plot -x pdf -l ${organism}_${gene} --mnum192 0
+# calculate_mutspec.py -b ${indir}/observed_mutations_iqtree.tsv -e ${indir}/exp_muts_invariant.tsv -o $indir/ms \
+#     --exclude OUTGRP,ROOT --proba  --syn --plot -x pdf -l ${organism}_${gene} --mnum192 0
 calculate_mutspec.py -b ${indir}/observed_mutations_iqtree.tsv -e ${indir}/exp_muts_invariant.tsv -o $indir/ms \
     --exclude OUTGRP,ROOT --proba  --syn --plot -x pdf -l ${organism}_${gene} --subset internal --mnum192 0
 
@@ -73,45 +72,32 @@ fi
 awk '/^>/ {P=index($1, "OUTGRP")==0} {if(P) print}' $raw_mulal > ${mulal}.ingroup
 echo "Tree outgroup sequence filtered out"
 ## filter out sequences with ambigous nucleotides
-echo > $log_file
 cat ${mulal}.ingroup | perl -e '$p=$s="777"; while (<STDIN>) {chomp; if ($_=~/^>/) {$h=$_; if ($s!~/[^ACGT]/i) {print "$p\n$s\n"} $p=$h; $s="777"} else {$s=$_}} if ($s!~/[^ACGT]/i) {print "$p\n$s\n"}' > ${mulal}.clean
 if [ `grep -c ">" ${mulal}.clean` -lt 1 ]; then
-	echo -e "There are no sequences without ambigous nucleotides in the alignment.\nInterrupted" > $log_file
+	echo -e "There are no sequences without ambigous nucleotides in the alignment.\nInterrupted"
 	exit 0
 fi
 
 pyvolve_process.py -a ${mulal}.clean -t ${tree}.ingroup \
-	-s $indir/ms/ms192syn_internal_${organism}_${gene}.tsv \
+	-s $indir/ms/ms12syn_internal_${organism}_${gene}.tsv \
 	-o $indir/pyvolve/seqfile.fasta -r $replics -c $GENCODE \
 	-l 1 --write_anc --rates $rates
 echo -e "Mutation samples generated\n"
 
+parallel --jobs $THREADS alignment2iqtree_states.py {} {}.state ::: $indir/pyvolve/seqfile_sample-*.fasta
 
-# parallel --dry-run alignment2iqtree_states.py {} {}.state ::: $indir/pyvolve/seqfile_sample-*.fasta
-# parallel --dry-run 3.collect_mutations.py --tree ${tree}.ingroup --states {}.state \
-# 		--gencode $GENCODE --syn --no-mutspec --outdir $indir/pyvolve/mout --quiet ::: $indir/pyvolve/seqfile_sample-*.fasta
+parallel --jobs $THREADS collect_mutations.py --tree ${tree}.ingroup --states {} --gencode $GENCODE --syn --no-mutspec \
+			 --outdir $indir/pyvolve/mout/{#} --force --quiet ::: $indir/pyvolve/seqfile_sample-*.fasta.state
 
-for fasta_file in $indir/pyvolve/seqfile_sample-*.fasta; do
-	echo "Processing $fasta_file"
-	alignment2iqtree_states.py $fasta_file $fasta_file.state
-	collect_mutations.py --tree ${tree}.ingroup --states ${fasta_file}.state \
-		--gencode $GENCODE --syn --no-mutspec --outdir $indir/pyvolve/mout --force --quiet
-	rm $fasta_file ${fasta_file}.state
-    cat $indir/pyvolve/mout/run.log >> $log_file
-	echo -e "\n\n">> $log_file
-	cat $indir/pyvolve/mout/mutations.tsv > $fasta_file.mutations
-done
+rm $indir/pyvolve/seqfile_sample-*.fasta $indir/pyvolve/seqfile_sample-*.fasta.state
 
-# if [ ! -f ${indir}/observed_mutations_iqtree.tsv ]; then
-concat_mutations.py $indir/pyvolve/seqfile_sample-*.fasta.mutations $indir/pyvolve/mutations_${method}_pyvolve.tsv
-rm $indir/pyvolve/seqfile_sample-*.fasta.mutations
+concat_mutations.py $indir/pyvolve/mout/*/mutations.tsv $indir/pyvolve/mutations_${method}_pyvolve.tsv
 echo "Mutations concatenation done"
-# else
-# echo "File "
 
-calculate_mutspec.py -b $indir/pyvolve/mutations_${method}_pyvolve.tsv -e $indir/exp_muts_invariant.tsv \
-	-o $indir/pyvolve/out -l ${organism}_${gene}_simulated --exclude OUTGRP,ROOT --syn --mnum192 0 --plot -x pdf \
-    --substract192 $indir/ms/ms192syn_${organism}_${gene}.tsv --substract12 $indir/ms/ms12syn_${organism}_${gene}.tsv
+
+# calculate_mutspec.py -b $indir/pyvolve/mutations_${method}_pyvolve.tsv -e $indir/exp_muts_invariant.tsv \
+# 	-o $indir/pyvolve/out -l ${organism}_${gene}_simulated --exclude OUTGRP,ROOT --syn --mnum192 0 --plot -x pdf \
+#     --substract192 $indir/ms/ms192syn_${organism}_${gene}.tsv --substract12 $indir/ms/ms12syn_${organism}_${gene}.tsv
 calculate_mutspec.py -b $indir/pyvolve/mutations_${method}_pyvolve.tsv -e $indir/exp_muts_invariant.tsv \
 	-o $indir/pyvolve/out -l ${organism}_${gene}_internal_simulated --exclude OUTGRP,ROOT --syn --mnum192 0 --plot -x pdf \
     --substract192 $indir/ms/ms192syn_internal_${organism}_${gene}.tsv --substract12 $indir/ms/ms12syn_internal_${organism}_${gene}.tsv
