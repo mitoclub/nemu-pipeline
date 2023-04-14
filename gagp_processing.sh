da4e25e25b0a4f024faae29a47c5de392aa7fbc9
kpotoh <axepon@mail.ru>
2023-04-14 16:23:43 +0200

    images swap & wiki images

--- gagp_processing.sh
+++
@@ -1,6 +0,0 @@
-
-#macse
-parallel --jobs 13 java -jar /home/kpotoh/tools/macse_v2.06.jar -prog alignSequences -gc_def 2 -seq {} -out_AA mulal/{/.}.faa -out_NT mulal/{/.}.fna ::: genes/*
-
-#iqtree
-parallel --jobs 13 iqtree2 -s {} -m GTR+FO+R6+I -nt 4 --prefix {/.} --rate ::: mulal/*.fna
