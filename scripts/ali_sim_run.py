e20819943f0519a09cab7bbae1d35a514c207da9
kpotoh <axepon@mail.ru>
2023-07-19 14:54:54 +0300

    alisim compare for sp done

--- scripts/ali_sim_run.py
+++ scripts/ali_sim_run.py
@@ -10,7 +10,7 @@ ROOT_SEQ_NAME = "human_cytb"
 LENGTH = 1140
 SEED = random.randint(0, 100000)
 
-debug_mode = False
+debug_mode = True
 OUTDIR = "generations_mam"
 N = 50
 
@@ -118,3 +118,17 @@ for mdl in ["gtr", "12.12"]:
                     cur_cmd = cmd.format(prefix, mdl, mdl_params, f_custom, inv_rate, pregenerated_tree, SEED, root_seq, "")
                     os.system(cur_cmd)
                     os.remove(prefix + ".full.treefile")
+
+
+# iqtree2 --alisim generations/gtr_100_rnd_replica_0 -m gtr{0.01/0.1/0.21/0.26/0.05/0.18}+F{0.08/0.35/0.35/0.23}+G6+I{0.48} -t RANDOM{bd{0.1/0.05}/100} --seed 96734 --write-all --length 1140 -af fasta -rlen 0 0.001 0.01
+# iqtree2 --alisim generations/gtr_1000_rnd_replica_0 -m gtr{0.11/0.32/0.26/0.94/0.23/0.25}+F{0.37/0.22/0.01/0.4}+G6+I{0.35} -t RANDOM{bd{0.1/0.05}/1000} --seed 96734 --write-all --length 1140 -af fasta -rlen 0 0.001 0.01
+# iqtree2 --alisim generations/gtr_mam_rnd_replica_0 -m gtr{0.03/0.26/0.09/0.14/0.06/0.08}+F{0.26/0.38/0.16/0.21}+G6+I{0.04} -t /home/kpotoh/nemu-pipeline/data/alisim/external/mam_with_outgrp.nwk --seed 96734 --write-all --length 1140 -af fasta 
+# iqtree2 --alisim generations/gtr_100_cytb_replica_0 -m gtr{0.3/0.16/0.19/0.26/0.01/0.04}+G6+I{0.35} -t RANDOM{bd{0.1/0.05}/100} --seed 96734 --write-all --root-seq /home/kpotoh/nemu-pipeline/data/alisim/external/human_cytb.fasta,human_cytb -af fasta -rlen 0 0.001 0.01
+# iqtree2 --alisim generations/gtr_1000_cytb_replica_0 -m gtr{0.1/0.06/0.06/0.22/0.3/0.1}+G6+I{0.2} -t RANDOM{bd{0.1/0.05}/1000} --seed 96734 --write-all --root-seq /home/kpotoh/nemu-pipeline/data/alisim/external/human_cytb.fasta,human_cytb -af fasta -rlen 0 0.001 0.01
+# iqtree2 --alisim generations/gtr_mam_cytb_replica_0 -m gtr{0.07/0.02/0.11/0.1/0.04/0.16}+G6+I{0.32} -t /home/kpotoh/nemu-pipeline/data/alisim/external/mam_with_outgrp.nwk --seed 96734 --write-all --root-seq /home/kpotoh/nemu-pipeline/data/alisim/external/human_cytb.fasta,human_cytb -af fasta 
+# iqtree2 --alisim generations/12.12_100_rnd_replica_0 -m 12.12{-0.27/0.04/-0.72/-0.62/-0.08/-0.57/-0.82/-0.4/0.07/0.28/0.97}+F{0.28/0.4/0.02/0.3}+G6+I{0.09} -t RANDOM{bd{0.1/0.05}/100} --seed 96734 --write-all --length 1140 -af fasta -rlen 0 0.001 0.01
+# iqtree2 --alisim generations/12.12_1000_rnd_replica_0 -m 12.12{-0.44/0.2/-0.13/0.37/0.8/-0.38/-0.25/-0.34/-0.52/0.45/0.56}+F{0.16/0.07/0.33/0.44}+G6+I{0.43} -t RANDOM{bd{0.1/0.05}/1000} --seed 96734 --write-all --length 1140 -af fasta -rlen 0 0.001 0.01
+# iqtree2 --alisim generations/12.12_mam_rnd_replica_0 -m 12.12{0.28/0.65/-0.13/0.25/0.5/-0.17/0.38/0.73/0.01/-0.78/0.31}+F{0.15/0.21/0.24/0.39}+G6+I{0.22} -t /home/kpotoh/nemu-pipeline/data/alisim/external/mam_with_outgrp.nwk --seed 96734 --write-all --length 1140 -af fasta
+# iqtree2 --alisim generations/12.12_100_cytb_replica_0 -m 12.12{-0.49/0.53/-0.13/-0.47/0.26/-0.41/-0.31/0.15/-0.27/0.63/-0.61}+G6+I{0.15} -t RANDOM{bd{0.1/0.05}/100} --seed 96734 --write-all --root-seq /home/kpotoh/nemu-pipeline/data/alisim/external/human_cytb.fasta,human_cytb -af fasta -rlen 0 0.001 0.01
+# iqtree2 --alisim generations/12.12_1000_cytb_replica_0 -m 12.12{0.16/-0.97/0.06/0.14/-0.21/-0.03/0.93/-0.07/0.83/0.47/-0.42}+G6+I{0.3} -t RANDOM{bd{0.1/0.05}/1000} --seed 96734 --write-all --root-seq /home/kpotoh/nemu-pipeline/data/alisim/external/human_cytb.fasta,human_cytb -af fasta -rlen 0 0.001 0.01
+# iqtree2 --alisim generations/12.12_mam_cytb_replica_0 -m 12.12{-0.16/0.15/0.92/0.39/-0.31/-0.45/-0.54/-0.43/-0.05/0.14/0.58}+G6+I{0.46} -t /home/kpotoh/nemu-pipeline/data/alisim/external/mam_with_outgrp.nwk --seed 96734 --write-all --root-seq /home/kpotoh/nemu-pipeline/data/alisim/external/human_cytb.fasta,human_cytb -af fasta
