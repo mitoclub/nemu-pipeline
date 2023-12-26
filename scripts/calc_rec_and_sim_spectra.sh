72924a5f29ec2cf71573594b592818e9725eb323
kpotoh <axepon@mail.ru>
2023-12-26 15:30:46 +0300

    selection search remembering

--- scripts/calc_rec_and_sim_spectra.sh
+++ scripts/calc_rec_and_sim_spectra.sh
@@ -1,8 +1,4 @@
-THREADS=64
-export MKL_NUM_THREADS=$THREADS
-export NUMEXPR_NUM_THREADS=$THREADS
-export OMP_NUM_THREADS=$THREADS
-
+# part of run_simulation.sh script
 
 for organism in mouse human; do
 for gene in nd1 cytb cox1; do
@@ -18,7 +14,6 @@ echo -e "\nInput directory: $indir"
 mkdir -p $indir/ms $indir/pyvolve/out
 
 method=iqtree
-replics=100
 GENCODE=2
 
 raw_tree=$indir/IQTREE/iqtree_anc_tree.nwk
@@ -47,45 +42,6 @@ calculate_mutspec.py -b $obs_mutations -e $exp_mutations -o $indir/ms \
 calculate_mutspec.py -b $obs_mutations -e $exp_mutations -o $indir/ms \
     --exclude OUTGRP,ROOT --proba --syn --plot -x pdf -l ${organism}_${gene} --subset internal --mnum192 0
 echo "Reconstructed mutational spectrum calculated"
-
-# tree processing
-nw_prune $raw_tree OUTGRP | python3 -c "import sys,re; print(re.sub('\d+\.\d+e-\d+', lambda m: '{:.10f}'.format(float(m.group())), sys.stdin.read().strip()))" > ${tree}.ingroup
-echo "Tree outgroup pruned"
-## replace digit-named nodes from RAxML
-nw_labels -L ${tree}.ingroup | grep -v ROOT | xargs -I{} echo -e "{}\tNode{}" > $indir/pyvolve/map.txt
-if [ `grep -c NodeNode $indir/pyvolve/map.txt` -eq 0 ]; then
-	nw_rename ${tree}.ingroup $indir/pyvolve/map.txt > ${tree}.tmp
-	cat ${tree}.tmp > ${tree}.ingroup
-	echo "Internal nodes renamed"
-fi
-
-# alignment processing
-awk '/^>/ {P=index($1, "OUTGRP")==0} {if(P) print}' $raw_mulal > ${mulal}.ingroup
-echo "Tree outgroup sequence filtered out"
-## filter out sequences with ambigous nucleotides
-cat ${mulal}.ingroup | perl -e '$p=$s="777"; while (<STDIN>) {chomp; if ($_=~/^>/) {$h=$_; if ($s!~/[^ACGT]/i) {print "$p\n$s\n"} $p=$h; $s="777"} else {$s=$_}} if ($s!~/[^ACGT]/i) {print "$p\n$s\n"}' > ${mulal}.clean
-if [ `grep -c ">" ${mulal}.clean` -lt 1 ]; then
-	echo -e "There are no sequences without ambigous nucleotides in the alignment.\nInterrupted"
-	exit 0
-fi
-
-pyvolve_process.py -a ${mulal}.clean -t ${tree}.ingroup \
-	-s $indir/ms/ms12syn_internal_${organism}_${gene}.tsv \
-	-o $indir/pyvolve/seqfile.fasta -r $replics -c $GENCODE \
-	-l 1 --write_anc --rates $rates
-echo -e "Mutation samples generated\n"
-
-parallel --jobs $THREADS alignment2iqtree_states.py {} {}.state ::: $indir/pyvolve/seqfile_sample-*.fasta
-
-# HERE I DIDN'T USE RATES, BECAUSE EXP DON'T USED IN SEQUENTIAL ANALYSIS AND OBS FILTERED IN NB.
-# BUT IN COMMON CASE WE MUST USE RATES HERE 
-parallel --jobs $THREADS collect_mutations.py --tree ${tree}.ingroup --states {} --gencode $GENCODE --syn --no-mutspec \
-			 --outdir $indir/pyvolve/mout/{#} --force --quiet ::: $indir/pyvolve/seqfile_sample-*.fasta.state
-
-rm $indir/pyvolve/seqfile_sample-*.fasta $indir/pyvolve/seqfile_sample-*.fasta.state
-
-concat_mutations.py $indir/pyvolve/mout/*/mutations.tsv $indir/pyvolve/mutations_${method}_pyvolve.tsv
-echo "Mutations concatenation done"
 
 
 # ALL BRANCHES
