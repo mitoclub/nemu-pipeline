af112b636ef24cac9bee09ac245e7d8561eba37c
kpotoh <axepon@mail.ru>
2024-03-27 23:18:00 +0300

    run cholerae script w\o site filtration

---
+++ scripts/run_bacteria_cholerae.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+PATH_TO_NEMU=/home/kpotoh/nemu-pipeline/pipeline/nemu-core.nf
+PATH_TO_INPUT=/home/askudnov/Updater_Outputs/s__Vibrio_cholerae3/output/result
+PATH_TO_OUTPUT=/home/kpotoh/nemu-pipeline/data/cholerae/output
+PATH_TO_CONFIG=/home/kpotoh/nemu-pipeline/data/cholerae/nemu_cholerae.config
+
+if [ ! -f $PATH_TO_NEMU ]
+then 
+    echo "$PATH_TO_NEMU doesn't exist"
+    exit 1
+fi
+if [ ! -f $PATH_TO_CONFIG ]
+then 
+    echo "$PATH_TO_CONFIG doesn't exist"
+    exit 1
+fi
+if [ ! -e $PATH_TO_OUTPUT ]
+then 
+    echo "$PATH_TO_OUTPUT doesn't exist"
+    exit 1
+fi
+if [ ! -e $PATH_TO_INPUT ]
+then 
+    echo "$PATH_TO_INPUT doesn't exist"
+    exit 1
+fi
+
+
+MAX_NJOBS=60
+SLEEP_TIME=120 # secs
+RUNLIMIT=2000
+COUNTER=1
+
+for species in `ls $PATH_TO_INPUT`; do
+    if [ $COUNTER -gt $RUNLIMIT ]; then
+        exit 0
+    fi
+
+    input_fasta=$PATH_TO_INPUT/$species
+    workdir=$PATH_TO_OUTPUT/$(basename $species .fasta)
+
+    if [ -e $workdir ]; then 
+        echo "'$workdir' already exist: pass"
+        continue
+    fi
+    let COUNTER++
+
+    echo -e "workdir: $workdir"
+    mkdir -p $workdir
+    cd $workdir
+    cp $input_fasta .
+
+    aln=$species
+
+    # -resume
+    # -q -bg
+    nextflow -bg -q -c $PATH_TO_CONFIG run $PATH_TO_NEMU -with-trace --sequence $aln --outdir .
+    cd - >/dev/null
+    sleep 0.5
+
+    if [ `expr $COUNTER % $MAX_NJOBS` -eq 0 ]; then
+        echo sleep $SLEEP_TIME secs
+        sleep $SLEEP_TIME
+    fi
+done
+echo
+
+echo "Processed $COUNTER fastas"  # 17748
+
+# rm -rf $PATH_TO_OUTPUT/*/*/*/work
